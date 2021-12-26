c74a76d3761527c99eb3108d839b58971af3c287
Bertus8 <albertinisp@gmail.com>
2021-12-26 11:38:37 +0100

    initial commit

---
+++ function/main.js
@@ -0,0 +1,9 @@
+const hole1Div$$ = document.querySelector(".hole hole1");
+const hole2Div$$ = document.querySelector(".hole hole2");
+const hole3Div$$ = document.querySelector(".hole hole3");
+const hole4Div$$ = document.querySelector(".hole hole4");
+const hole5Div$$ = document.querySelector(".hole hole5");
+const hole6Div$$ = document.querySelector(".hole hole6");
+
+const moleDiv$$ = document.querySelectorAll(".mole");
+const scoreSpan = document.querySelector(".score");
