8ff8768d319f1d595fc57a519eb1f8b9bd727c29
Bertus8 <albertinisp@gmail.com>
2021-12-28 12:18:50 +0100

    random mole

--- function/main.js
+++ function/main.js
@@ -1,3 +1,4 @@
+const gameDiv$$ = document.querySelector(".game");
 const hole1Div$$ = document.querySelector(".hole hole1");
 const hole2Div$$ = document.querySelector(".hole hole2");
 const hole3Div$$ = document.querySelector(".hole hole3");
@@ -5,5 +6,28 @@ const hole4Div$$ = document.querySelector(".hole hole4");
 const hole5Div$$ = document.querySelector(".hole hole5");
 const hole6Div$$ = document.querySelector(".hole hole6");
 
-const moleDiv$$ = document.querySelectorAll(".mole");
+const moleDivs$$ = document.querySelectorAll(".mole");
 const scoreSpan = document.querySelector(".score");
+
+
+let lasthole = null;
+let timeGame = true;
+let score = 0;
+
+const randomTimeMoleInterval = setInterval(randomTimeMole,500);
+
+
+const randomTimeMole = () => {
+for (const moleDiv$$ of moleDivs$$) {
+    moleDiv$$.classList.remove('mole')
+}
+
+ const randomMole = Math.floor(Math.random() * 6);
+ let position = moleDivs$$[randomMole]
+ position.classList.add('mole')
+ lasthole = randomMole
+}
+
+
+
+
